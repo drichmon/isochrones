fc652df0e26fd615c1dcfd015400c2b91a33db78
Dylan <dylan@turtle.com>
2017-07-04 00:34:59 +0200

    Changes made to Dartmouth to compile all isochrones in single FITS file. Still lingering issues with a few of the metallicities at the beginning of each isochrone

--- Dartmouthpreparer.py
+++ Dartmouthpreparer.py
@@ -5,8 +5,8 @@ import argparse
 from astropy.io import fits
 
 
-inpath = '/home/richmond/Isochrones/Dartmouth/interpolated/' #path to isochrone files
-outpath = '/home/richmond/Isochrones/FITS/' #desired output for FITS files
+inpath = '/home/dylan/Isochrones/Dartmouth/' #path to isochrone files
+outpath = '/home/dylan/Isochrones/FITS/' #desired output for FITS files
 
 def find_Metallicity(Dartmouth_file):
     i = 0
@@ -52,35 +52,48 @@ def get_skiprows(inputAge, Dartmouth_file):
             return i
     Dartmouth_file.close()
 
-def fitsTable(listOfAges, metallicity, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath):
+def CompiledAges(listOfAges, initial_mass, columns):
+    finalAges = []
     i = 0
     j = 1
     k = 0
-    metallicityList = []
-    Ages = listOfAges
-    finalAges = []
-    for item in initial_mass:
+    for item in columns:
         np.set_printoptions(precision=14)
         try:
-            if EEP[j] < EEP[i]:
-                nextAge = Ages[k]
+            if columns[j, 0] - columns[i, 0] <0:
+                nextAge = listOfAges[k]
                 k += 1
                 j += 1
+                #print nextAge
                 finalAges.append(nextAge)
-                metallicityList.append(metallicity)
+                #metallicityList.append(metallicity)
                 i += 1
-            elif EEP[j] > EEP[i]:
-                nextAge = Ages[k]
+            elif columns[j, 0] - columns[i, 0] >0 :
+                nextAge = listOfAges[k]
                 j += 1
+                #print nextAge
                 finalAges.append(nextAge)
-                metallicityList.append(metallicity)
+                #metallicityList.append(metallicity)
                 i += 1
             else:
                 break
         except (IndexError):
             pass    
-    x = np.array(finalAges)
-    metallicityArray = np.array(metallicityList)
+    return finalAges
+
+
+
+
+
+def fitsTable(allAges, metallicitylist, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath):
+    metallicityList = np.array(metallicitylist)   
+    x = np.array(allAges) 
+    initial_mass = np.array(initial_mass)
+    Teff = np.array(Teff)
+    log_g = np.array(log_g)
+    _2Mass_J = np.array(_2Mass_J)
+    _2Mass_H = np.array(_2Mass_H)
+    _2Mass_Ks = np.array(_2Mass_Ks)
     col1 = fits.Column(name='logAge', format='E', array=x)
     col2 = fits.Column(name='Metallicity', format='E', array=metallicityList)
     col3 = fits.Column(name='Teff', format='E', array=Teff)
@@ -91,27 +104,44 @@ def fitsTable(listOfAges, metallicity, initial_mass, Teff, log_g, _2Mass_J, _2Ma
     col8 =  fits.Column(name='Ks', format='E', array=_2Mass_Ks)
     cols = fits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8])
     tbhdu = fits.BinTableHDU.from_columns(cols)
-    tbhdu.writeto(outpath + 'Dartmouth_%s.fits' % metallicity, clobber=True) #fits outfile
+    tbhdu.writeto(outpath + 'Dartmouth.fits', clobber=True) #fits outfile
 
+EEP = []
+initial_mass = [] 
+log_Teff = []
+log_g = []
+_2Mass_J = []
+_2Mass_H = []
+_2Mass_Ks = []
+Teff = []
+metallicitylist = []
+allAges = []
 
-inputAge = str(1.000) #raw_input('')
+inputAge = str(1.000)
 for file in sorted(os.listdir(inpath)):
     file1 = file
-    Dartmouth_file = open(inpath +file1, 'r')
-    num_skip_rows = get_skiprows(inputAge, Dartmouth_file)
-    x = num_skip_rows - 1
-    EEP = np.loadtxt(inpath +file1, skiprows = x, usecols = (0,))
-    initial_mass = np.loadtxt(inpath +file1, skiprows = x, usecols = (1,))
-    log_Teff = np.loadtxt(inpath +file1, skiprows = x, usecols = (2,))
-    log_g = np.loadtxt(inpath +file1, skiprows = x, usecols = (3,))
-    _2Mass_J = np.loadtxt(inpath +file1, skiprows = x, usecols = (10,))
-    _2Mass_H = np.loadtxt(inpath +file1, skiprows = x, usecols = (11,))
-    _2Mass_Ks = np.loadtxt(inpath +file1, skiprows = x, usecols = (12,))
-    Teff = log_teff_to_teff(log_Teff)
+    x = 5
+    columns = np.loadtxt(inpath + file1)
+    EEP.extend(columns[:, 0])
+    initial_mass.extend(columns[:, 1])
+    log_Teff.extend(columns[:, 2])
+    log_g.extend(columns[:, 3])
+    _2Mass_J.extend(columns[:, 10])
+    _2Mass_H.extend(columns[:, 11])
+    _2Mass_Ks.extend(columns[:, 12])
     Dartmouth_file = open(inpath +file1, 'r')
     metallicity = find_Metallicity(Dartmouth_file)
     Dartmouth_file = open(inpath +file1, 'r')
-    listOfAges = getAge(Dartmouth_file, x)
+    for item in columns:
+        metallicitylist.append(metallicity)
+    print len(metallicitylist)
     Dartmouth_file = open(inpath +file1, 'r')
+    listOfAges = getAge(Dartmouth_file, x)
     print file1 + '\t' + metallicity
-    fitsTable(listOfAges, metallicity, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath)
+    Ages = CompiledAges(listOfAges, initial_mass, columns)
+    allAges.extend(Ages)
+
+Teff.extend(np.power(10, log_Teff))
+print len(Teff)
+fitsTable(allAges, metallicitylist, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath)
+
