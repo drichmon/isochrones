9d6c7d332d673e8cba493fe57239d795df0165cd
Dylan <dylan@turtle.com>
2017-07-04 15:55:27 +0200

    Working FITS file preparer for isochrones

--- PARSECpreparer.py
+++ PARSECpreparer.py
@@ -26,17 +26,13 @@ def log_teff_to_teff(log_Teff):
 def getAge(PARSEC_file, x):
     Ages = []
     i = 0
-    y = x - 5 # didn't really feel like figuring out the exact difference
     term = "Age ="
     with PARSEC_file as infile:
         for line in infile:
-            if i < y:
-                i += 1
-            elif i >= y:
-                if term in line:
-                    #print line[72:78] + '\t' + line[80:83]
-                    Age = float(line[72:78]) * 10 ** int(line[80:83]) #PREVIOUSLY +1 TO ALL INDICIES IN THIS LINE
-                    Ages.append(Age)
+            if term in line:
+                #print line[72:78] + '\t' + line[80:83]
+                Age = line[72:83]#float(line[72:78]) * 10 ** int(line[80:83]) #PREVIOUSLY +1 TO ALL INDICIES IN THIS LINE
+                Ages.append(Age)
     return Ages
     PARSEC_file.close()
 
@@ -51,34 +47,45 @@ def get_skiprows(inputAge, PARSEC_file):
             return i
     PARSEC_file.close()
 
-def fitsTable(listOfAges, metallicity, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath):
+def CompiledAges(listOfAges, initial_mass, columns):
+    finalAges = []
     i = 0
     j = 1
     k = 0
-    metallicityList = []
-    Ages = listOfAges
-    finalAges = []
-    for item in initial_mass:
+    for item in columns:
         np.set_printoptions(precision=14)
         try:
-            if initial_mass[j] < initial_mass[i]:
-                nextAge = Ages[k]
+            if columns[j, 1] != columns[i, 1]:
                 k += 1
+                nextAge = listOfAges[k]
                 j += 1
-                finalAges.append(float(nextAge))
-                metallicityList.append(metallicity)
+                #print nextAge
+                finalAges.append(nextAge)
+                #metallicityList.append(metallicity)
                 i += 1
-            elif initial_mass[j] >= initial_mass[i]:
-                nextAge = Ages[k]
+            elif columns[j, 1] == columns[i, 1]:
+                nextAge = listOfAges[k]
                 j += 1
-                finalAges.append(float(nextAge))
-                metallicityList.append(metallicity)
+                #print nextAge
+                finalAges.append(nextAge)
+                #metallicityList.append(metallicity)
                 i += 1
             else:
+                finalAges.append(nextAge)
                 break
         except (IndexError):
-            pass
-    x = np.array(finalAges)
+            pass    
+    return finalAges
+
+def fitsTable(allAges, metallicitylist, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath):
+    metallicityList = np.array(metallicitylist)   
+    x = np.array(allAges) 
+    initial_mass = np.array(initial_mass)
+    Teff = np.array(Teff)
+    log_g = np.array(log_g)
+    _2Mass_J = np.array(_2Mass_J)
+    _2Mass_H = np.array(_2Mass_H)
+    _2Mass_Ks = np.array(_2Mass_Ks)
     metallicityArray = np.array(metallicityList)
     col1 = fits.Column(name='logAge', format='E', array=x)
     col2 = fits.Column(name='Metallicity', format='E', array=metallicityList)
@@ -90,26 +97,40 @@ def fitsTable(listOfAges, metallicity, initial_mass, Teff, log_g, _2Mass_J, _2Ma
     col8 =  fits.Column(name='Ks', format='E', array=_2Mass_Ks)
     cols = fits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8])
     tbhdu = fits.BinTableHDU.from_columns(cols)
-    tbhdu.writeto(outpath + 'PARSEC_%s.fits' % metallicity, clobber=True) #fits outfile
+    tbhdu.writeto(outpath + 'PARSEC.fits', clobber=True) #fits outfile
 
-inputAge = str(4.0738) #raw_input('')
+#inputAge = str(4.0738) #raw_input('')
+initial_mass = [] 
+log_Teff = []
+log_g = []
+_2Mass_J = []
+_2Mass_H = []
+_2Mass_Ks = []
+Teff = []
+metallicitylist = []
+allAges = []
+
 for file in sorted (os.listdir(inpath)): 
-    file1 = file
-    if '.dat' in file1: #makes sure we only iterate over the isochrone files
-        PARSEC_file = open(inpath +file1, 'r')
-        num_skip_rows = get_skiprows(inputAge, PARSEC_file)
-        x = num_skip_rows -1
-        initial_mass = np.loadtxt(inpath +file1, skiprows = x, usecols = (2,))
-        log_Teff = np.loadtxt(inpath +file1, skiprows = x, usecols = (5,))
-        log_g = np.loadtxt(inpath +file1, skiprows = x, usecols = (6,))
-        _2Mass_J = np.loadtxt(inpath +file1, skiprows = x, usecols = (13,))
-        _2Mass_H = np.loadtxt(inpath +file1, skiprows = x, usecols = (14,))
-        _2Mass_Ks = np.loadtxt(inpath +file1, skiprows = x, usecols = (15,))
-        Teff = log_teff_to_teff(log_Teff)
-        PARSEC_file = open(inpath +file1, 'r')
+    if '.dat' in file: #makes sure we only iterate over the isochrone files
+        x = 5
+        columns = np.loadtxt(inpath + file)
+        initial_mass.extend(columns[:, 2])
+        log_Teff.extend(columns[:, 5])
+        log_g.extend(columns[:, 6])
+        _2Mass_J.extend(columns[:, 13])
+        _2Mass_H.extend(columns[:, 14])
+        _2Mass_Ks.extend(columns[:, 15])
+        allAges.extend(columns[:, 1])
+        PARSEC_file = open(inpath +file, 'r')
         metallicity = find_Metallicity(PARSEC_file)
-        PARSEC_file = open(inpath +file1, 'r')
-        listOfAges = getAge(PARSEC_file, x)
-        PARSEC_file = open(inpath +file1, 'r')
-        print file1 + '\t' + metallicity
-        fitsTable(listOfAges, metallicity, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath)
+        for item in columns:
+            metallicitylist.append(metallicity)
+        print file + '\t' + metallicity
+
+        
+
+Teff.extend(np.power(10, log_Teff))
+#allAges = np.power(10, allAges) #uncomment for linear ages, also change name of fits column from logAge ---> Age
+fitsTable(allAges, metallicitylist, initial_mass, Teff, log_g, _2Mass_J, _2Mass_H, _2Mass_Ks, outpath)
+
+print 'FITS file created for PARSEC'
